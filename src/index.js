8a3d198b2f7e3511ec0737aa1cbbe0e3c39f4575
Ivan Kaznadzhiev <i.kazandjiev@gmail.com>
2017-07-10 16:53:50 +0300

    Add electron autoreloading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const electron = require('electron')
+const { app, BrowserWindow } = require('electron')
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow
+require('electron-reload')(__dirname, {
+  electron: require('electron-prebuilt')
+});
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
-
+  
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   mainWindow.on('closed', () => {
