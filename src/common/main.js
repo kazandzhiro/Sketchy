67a1d5d3c1cb8f1b35a02b4fb56eee45210eda9c
Ivan Kaznadzhiev <i.kazandjiev@gmail.com>
2017-07-14 16:05:44 +0300

    Adds stability to this dirty proto

--- src/common/main.js
+++ src/common/main.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const { remote: { dialog, getGlobal, getCurrentWindow } } = require('electron');
 const conf = getGlobal('shared').conf;
-const imagesPath = conf.get('imagesPath');
-const imagesTree = conf.get('imagesTree');
+let imagesPath = conf.get('imagesPath');
+let imagesTree = conf.get('imagesTree');
 let time;
 let startTime;
+let intervalId;
+let isPaused = false;
+let changeImg;
 
 const onInit = () => {
   const pathInput = document.getElementById('path');
@@ -25,10 +28,14 @@ const onAddImgPath = evt => {
     }) || []).shift();
 
   if (imgFolderPath && fs.existsSync(imgFolderPath)) {
+    conf.delete('imagesPath');
+    conf.delete('imagesTree');
     conf.set('imagesPath', imgFolderPath);
     const pathInput = document.getElementById('path');
     pathInput.value = imgFolderPath;
     conf.set('imagesTree', constructImgTree(imgFolderPath));
+    imagesPath = conf.get('imagesPath');
+    imagesTree = conf.get('imagesTree');
   }
 
   listImgDirs();
@@ -53,6 +60,7 @@ const constructImgTree = (parentDir, imgTree = {}) => {
 const listImgDirs = () => {
   const imgFolderElement = document.getElementsByClassName('img-folders')[0];
   const folderNames = Object.keys(conf.get('imagesTree'));
+  imgFolderElement.innerHTML = '';
   folderNames.forEach( folderName => imgFolderElement.innerHTML += `<div class="ui checkbox">
     <input name="imgfolders" type="checkbox" value="${folderName}" checked>
    <label>${folderName}</label>
@@ -63,7 +71,9 @@ const startPractice = (evt) => {
   evt.preventDefault();
   // const settings = {};
   const form = document.getElementById('form');
-  const imgFolders = Array.from(form.elements.imgfolders)
+  const foldersArr = form.elements.imgfolders.length > 1 ?
+    form.elements.imgfolders : [form.elements.imgfolders];
+  const imgFolders = Array.from(foldersArr)
     .filter(chkbx => chkbx.checked)
     .map(chkbx => chkbx.value);
   const imgArray = imgFolders.reduce((acc, imgFolder) => {
@@ -74,8 +84,10 @@ const startPractice = (evt) => {
   document.getElementById('settings').style.display = 'none';
   document.getElementById('sketches').style.display = 'block';
 
-  startTime = startTimer(time, updateImage(imgArray));
+  changeImg = updateImage(imgArray);
+  startTime = startTimer(time, changeImg);
   startTime();
+  document.addEventListener('keyup', keyBoardNav);
 }
 
 const addFullPathToImg = (folder, images) => {
@@ -90,13 +102,19 @@ const updateImage = (imgArray) => {
   const imgEl = document.getElementById('sketch');
   imgEl.setAttribute('src', imgArray[0]);
   let i = 1;
-  return () => {
+  return (back) => {
+      if (back && i > 0) { i--;}
       if(i > imgArray.length -1) {
         window.alert('Lets start a new session');
         getCurrentWindow().reload();
+        document.removeEventListener('keypress', keyBoardNav);
       }
       imgEl.setAttribute('src', imgArray[i++]);
-      imgEl.onload = () => { startTime() }
+      imgEl.onload = () => { 
+        document.body.style.backgroundColor = '#fff';
+        document.getElementById('sketches').style.visibility = 'visible';
+        startTime() 
+      }
     }
 }
 
@@ -109,25 +127,37 @@ const startTimer = (time, changeImg) => {
 const timerTick = (time, changeImg) => {
   const timerEl = document.getElementById('timer');
   timerEl.innerHTML = `${time.minutes}:${time.seconds}`;
-  const intervalId = setInterval(() => {
+  intervalId = setInterval(() => {
+    if(isPaused) return;
     time.seconds--;
-    if(time.minutes === 0 && time.seconds === 0) {
+    if(time.minutes <= 0 && time.seconds <= 0) {
       clearInterval(intervalId);
       document.getElementById('sketches').style.visibility = 'hidden';
       document.body.style.backgroundColor = '#000';
       setTimeout(() => {
-        document.body.style.backgroundColor = '#fff';
-        document.getElementById('sketches').style.visibility = 'visible';
         changeImg();
       }, 2000);
     }
-    if(time.seconds === 0 && time.minutes > 0) {
+    if(time.seconds <= 0 && time.minutes > 0) {
       time.minutes--;
+      time.seconds = 59;
     }
     timerEl.innerHTML = `${time.minutes}:${time.seconds}`;
   }, 1000);
 }
- 
+
+const keyBoardNav = (evt) => {
+  if (evt.code === 'Space') isPaused = !isPaused;
+  if (evt.code === 'ArrowLeft') {
+    clearInterval(intervalId);
+    changeImg(true);
+  }
+  if (evt.code === 'ArrowRight') {
+    clearInterval(intervalId);
+    changeImg();
+  }
+}
+
 module.exports = {
   // onPathAdd,
   onInit,
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow } = require('electron');
 const conf = new (require('electron-store'))();
 
-require('electron-reload')(__dirname, {
-  electron: require('electron-prebuilt')
-});
+// require('electron-reload')(__dirname, {
+//   electron: require('electron-prebuilt')
+// });
 
 global.shared = {
   conf
@@ -12,10 +12,10 @@ let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 1800,
-    height: 1000,
-    x: 1920,
-    y: 0
+    width: 1280,
+    height: 768,
+    // x: 1920,
+    // y: 0
   });
 
   // conf.set('imagesPath', 'C:\\Users\\Ivan_Kazandzhiev\\Desktop\\images')
@@ -24,8 +24,9 @@ app.on('ready', () => {
   // const pathInput = document.getElementById('path');
   // pathInput.value = conf.imagePath || "Paste images path or...";
 
-  mainWindow.openDevTools();
+  // mainWindow.openDevTools();
   mainWindow.on('closed', () => {
+    app.quit();
     mainWindow = null;
   });
 });
